20321d164f97b43038077ee1bdde4fccccaa4fee
xavychin <xavychin@gmail.com>
2025-08-17 14:32:26 +0800

    Add functions to Leo
    Added simple functions that
    can be carried out by Leo when prompted

--- src/main/java/Leo.java
+++ src/main/java/Leo.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Leo {
     public static void main(String[] args) {
         Messages message = new Messages();
         message.Greetings();
-        message.Goodbye();
+
+        //Solution below adapted from https://www.perplexity.ai/search/how-to-obtain-user-input-in-cl-KvQBaeNySBqYFdBIYZJrjw
+        Scanner sc = new Scanner(System.in);
+        Functions func = new Functions();
+        //Solution of using equalsIgnoreCase() suggested by IntelliJ code completion
+        String CallFunction = "";
+        while(!CallFunction.equalsIgnoreCase("bye")){
+            CallFunction = sc.nextLine().toLowerCase();
+            func.SearchFunctions(CallFunction);
+            message.MessageBreak();
+        }
     }
 }
-:wq
