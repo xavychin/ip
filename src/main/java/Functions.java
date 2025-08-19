98a904fcf1d58042d41743aeef6082b90b7a08e5
xavychin <xavychin@gmail.com>
2025-08-19 18:26:30 +0800

    Update Functions.java
    -Modified SearchFunctions
    -Added new markAsDone function
    -Added new unmark function

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -2,23 +2,27 @@ import java.util.ArrayList;
 
 public class Functions {
     Messages message = new Messages();
-    ArrayList<String> listItems = new ArrayList<>();
+    ArrayList<Task> listItems = new ArrayList<>();
 
     public void SearchFunctions(String funcName){
-        switch(funcName){
+        String[] funcNameList = funcName.split(" ");
+        switch(funcNameList[0]){
             case "list":
                 this.list();
-                break;
-            case "blah":
-                this.blah();
                 break;
             case "bye":
                 message.Goodbye();
                 break;
+            case "mark":
+                this.markAsDone(Integer.parseInt(funcNameList[1]));
+                break;
+            case "unmark":
+                this.unmark(Integer.parseInt(funcNameList[1]));
+                break;
             default:
                 //System.out.println("\tI don't know how to do this...");
                 System.out.println("\tadded: " + funcName);
-                listItems.add(funcName);
+                listItems.add(new Task(funcName));
                 break;
         }
     }
@@ -26,11 +30,21 @@ public class Functions {
     public void list(){
         System.out.println("Here are the tasks in your list:");
         for(int i=0; i<listItems.size(); i++){
-            System.out.println("\t" + (i+1) + ". " + listItems.get(i));
+            System.out.println("\t" + (i+1) + ". " + listItems.get(i).toString());
         }
     }
 
-    public void blah(){
-        System.out.println("\tblah");
+    public void markAsDone(int num){
+        Task task = listItems.get(num-1);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t" + task.toString());
+    }
+
+    public void unmark(int num){
+        Task task = listItems.get(num-1);
+        task.unmark();
+        System.out.println("Ok! I've marked this task as not done yet:");
+        System.out.println("\t" + task.toString());
     }
 }
