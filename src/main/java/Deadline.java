094a571f853e8eabb0e3a75a3fb8f6e6649f89f5
xavychin <xavychin@gmail.com>
2025-08-23 23:14:26 +0800

    Update files to handle datetime input

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,23 @@
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
     private String deadline;
 
-    public Deadline(String description, String deadline){
+    public Deadline(String description, String deadline) throws DateTimeException{
         super(description);
-        this.deadline = deadline;
+
+        try{
+            //Solution adapted from https://www.perplexity.ai/search/can-localdatetime-parse-days-o-Ub7ZJIDuRtifbzHjhcOC9Q
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+            LocalDateTime dateTime = LocalDateTime.parse(deadline, formatter);
+            DateTimeFormatter newFormat = DateTimeFormatter.ofPattern("MMM dd yyyy, HHmm");
+            this.deadline = dateTime.format(newFormat);
+        } catch (DateTimeException e) {
+            throw new DateTimeException("Incorrect date or time format for /from or /to..." +
+                    "\n\tIt should be <dd/MM/yyyy HHmm>");
+        }
     }
 
     @Override
