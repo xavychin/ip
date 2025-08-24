0ab997cf940ed5b98b17ca9b55ab65aff3dbdfaf
xavychin <xavychin@gmail.com>
2025-08-24 14:39:03 +0800

    Add new command classes for a OOP approach

---
+++ src/main/java/ListTaskCommand.java
@@ -0,0 +1,14 @@
+public class ListTaskCommand implements Command {
+    //Solution adapted from https://www.perplexity.ai/search/how-to-create-a-custom-excepti-Y_RyDVATSjKGxzSDx1HUeg
+    public static void list(TaskList listItems) throws ZeroLengthException {
+        int listItemsLength = listItems.getSize();
+        if(listItemsLength == 0){
+            throw new ZeroLengthException("The list is empty.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for(int i=0; i<listItemsLength; i++){
+                System.out.println("\t" + (i+1) + ". " + listItems.getItemAtIndex(i).toString());
+            }
+        }
+    }
+}
