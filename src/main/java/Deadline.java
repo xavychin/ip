eb8487429e395f00530c198f72c101ee0e698a3d
xavychin <xavychin@gmail.com>
2025-08-24 14:33:07 +0800

    Add DateTimeParser.java to handle DateTime input
    DateTimeParser.java:
    -Handle DateTime input from user
    -Handle DateTime input from file that stores tasks
    -Formats the DateTime input based on user or file input
    
    DeadLine.java, Event.java:
    -Moved DateTime formatting to DateTimeParser.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,33 +1,28 @@
-import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Deadline extends Task{
     private String deadline;
 
-    public Deadline(String description, String deadline) throws DateTimeException{
+    public Deadline(String description, String deadline){
         super(description);
 
-        try{
-            //Solution adapted from https://www.perplexity.ai/search/can-localdatetime-parse-days-o-Ub7ZJIDuRtifbzHjhcOC9Q
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-            LocalDateTime dateTime = LocalDateTime.parse(deadline, formatter);
-            DateTimeFormatter newFormat = DateTimeFormatter.ofPattern("MMM dd yyyy, HHmm");
-            this.deadline = dateTime.format(newFormat);
-        } catch (DateTimeException e) {
-            throw new DateTimeException("Incorrect date or time format for /from or /to..." +
-                    "\n\tIt should be <dd/MM/yyyy HHmm>");
-        }
+        DateTimeParser dateTimeParser = new DateTimeParser();
+        this.deadline = dateTimeParser.dateTimeFormatter(deadline);
     }
 
     @Override
     public String appendToFile() {
-        return String.format("Deadline | " + this.getStatusIcon() + " | " + this.description + " | " + this.deadline);
+        return String.format("Deadline | "
+                + this.getStatusIcon()
+                + " | "
+                + this.description
+                + " | "
+                + this.deadline);
     }
 
     @Override
-    public String toString(){
-        return String.format("[D][" + this.getStatusIcon() + "] "
+    public String toString() {
+        return String.format("[D]["
+                + this.getStatusIcon()
+                + "] "
                 + this.description
                 + " (by: "
                 + this.deadline
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,34 +1,29 @@
-import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Event extends Task{
     private String startDate;
     private String endDate;
 
-    public Event(String description, String startDate, String endDate) throws DateTimeException{
+    public Event(String description, String startDate, String endDate){
         super(description);
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
-            LocalDateTime startDateTime = LocalDateTime.parse(startDate, formatter);
-            LocalDateTime endDateTime = LocalDateTime.parse(endDate, formatter);
 
-            DateTimeFormatter newFormat = DateTimeFormatter.ofPattern("MMM dd yyyy, HHmm");
-            this.startDate = startDateTime.format(newFormat);
-            this.endDate = endDateTime.format(newFormat);
-        } catch (DateTimeException e) {
-            throw new DateTimeException("Incorrect date ot time format for /from or /to..." +
-                    "\n\tIt should be <dd/MM/yyyy HHmm>");
-        }
+        DateTimeParser dateTimeParser = new DateTimeParser();
+        this.startDate = dateTimeParser.dateTimeFormatter(startDate);
+        this.endDate = dateTimeParser.dateTimeFormatter(endDate);
     }
 
     @Override
     public String appendToFile() {
-        return String.format("Event | " + this.getStatusIcon() + " | " + this.description + " | " + this.startDate + "-" + this.endDate);
+        return String.format("Event | "
+                + this.getStatusIcon()
+                + " | "
+                + this.description
+                + " | "
+                + this.startDate
+                + "-"
+                + this.endDate);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("[E]["
                 + this.getStatusIcon()
                 + "] "
