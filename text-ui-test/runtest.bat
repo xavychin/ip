2f8d807505e9569f7e7a566f14f1821ebbbc4a57
xavychin <xavychin@gmail.com>
2025-08-24 16:28:51 +0800

    Update runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,6 +3,13 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
+REM clear Leo.txt before testing to avoid errors
+echo.> ..\.data\Leo.txt
+
+REM Solution adapted from https://www.perplexity.ai/search/change-branch-name-in-local-an-Fkk8ZHiwSyOZ4IElmJrh_g#11
+REM create list of all .java files recursively
+dir /s /b ..\src\main\java\*.java > sources.txt
+
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
@@ -20,6 +27,9 @@ IF ERRORLEVEL 1 (
     exit /b 1
 )
 REM no error here, errorlevel == 0
+
+REM clear sources.txt
+echo.> sources.txt
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Leo.Leo < input.txt > ACTUAL.TXT
