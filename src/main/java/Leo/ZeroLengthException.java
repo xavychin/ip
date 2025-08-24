a92c861eb6914ab1973c12cc9319ac886a3d8a45
xavychin <xavychin@gmail.com>
2025-08-24 16:07:15 +0800

    Move files to new package Leo

--- src/main/java/Leo/ZeroLengthException.java
+++ src/main/java/Leo/ZeroLengthException.java
@@ -1,3 +1,5 @@
+package Leo;
+
 //Solution adapted from https://www.perplexity.ai/search/how-to-create-a-custom-excepti-Y_RyDVATSjKGxzSDx1HUeg
 public class ZeroLengthException extends Exception{
     public ZeroLengthException(){
