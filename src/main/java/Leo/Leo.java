af0fbdafab5fc746b709f7aa2bb41c6ec9c99694
xavychin <xavychin@gmail.com>
2025-08-28 17:03:51 +0800

    Fix error messsage not being output

--- src/main/java/Leo/Leo.java
+++ src/main/java/Leo/Leo.java
@@ -1,9 +1,11 @@
 package Leo;
 
+import Leo.Display.Messages;
 import Leo.Display.UI;
 import Leo.Functions.Functions;
 import Leo.Functions.Task.TaskList;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Leo {
@@ -12,13 +14,17 @@ public class Leo {
     private TaskList tasks;
     private Functions functions;
 
-    public Leo(String filePath) {
+    public Leo(String filePath) throws IOException {
         this.ui = new UI();
         this.fileHandler = new FileHandler(filePath);
         try {
             this.tasks = new TaskList(fileHandler.loadFile());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IOException("File not found and failed to create.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Messages.MessageBreak();
+            System.out.println(e.getMessage());
+            Messages.MessageBreak();
         }
     }
 
@@ -27,7 +33,7 @@ public class Leo {
         this.ui.getUserInput(this.functions);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Leo(".data/Leo.txt").run();
     }
 }
